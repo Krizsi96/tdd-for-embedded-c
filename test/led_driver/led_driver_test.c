09fa195ac0bebfda6d7d2ea442a12fbb27315a49
Kristof Kovacs <A0075075@kiongroup.com>
2023-11-17 21:13:42 +0100

    DOC:: write down test list for led driver

--- test/led_driver/led_driver_test.c
+++ test/led_driver/led_driver_test.c
@@ -7,3 +7,14 @@ TEST_SETUP(LedDriver) {}
 TEST_TEAR_DOWN(LedDriver) {}
 
 TEST(LedDriver, LedsOffAfterCreate) { TEST_FAIL_MESSAGE("Start here"); }
+
+// LED Driver Tests
+// All LEDS are off after the driver is intitialized
+// A single LED can be turned on
+// A single LED can be turned off
+// Multiple LEDs can be turned on/off
+// Turn on all LEDs
+// Turn off all LEDs
+// Query LED state
+// Check boundary values
+// Check out-of-bounds values
