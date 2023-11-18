e9a7b255b29bc517db9992fbdcfb49f8d55d5869
Kristof Kovacs <A0075075@kiongroup.com>
2023-11-18 16:02:58 +0100

    FEAT:: initialize all the leds off

--- include/led_driver.h
+++ include/led_driver.h
@@ -1,4 +1,9 @@
 #ifndef LED_DRIVER_H_
 #define LED_DRIVER_H_
 
+#include <stdint.h>
+
+void LedDriver_Create(uint16_t *address);
+void LedDriver_Destroy(void);
+
 #endif  // LED_DRIVER_H_
--- src/led_driver/led_driver.c
+++ src/led_driver/led_driver.c
@@ -1,1 +1,5 @@
 #include "led_driver.h"
+
+void LedDriver_Create(uint16_t *address) { *address = 0; }
+
+void LedDriver_Destroy(void) {}
