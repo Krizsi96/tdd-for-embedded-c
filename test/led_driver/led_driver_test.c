659c77e4d6865992ae61989610866240f2aced20
Kristof Kovacs <A0075075@kiongroup.com>
2023-11-18 16:02:43 +0100

    TEST:: add test case for initialization

--- test/led_driver/led_driver_test.c
+++ test/led_driver/led_driver_test.c
@@ -1,3 +1,5 @@
+#include "led_driver.h"
+
 #include "unity_fixture.h"
 
 TEST_GROUP(LedDriver);
@@ -6,10 +8,14 @@ TEST_SETUP(LedDriver) {}
 
 TEST_TEAR_DOWN(LedDriver) {}
 
-TEST(LedDriver, LedsOffAfterCreate) { TEST_FAIL_MESSAGE("Start here"); }
-
 // LED Driver Tests
 // All LEDS are off after the driver is intitialized
+TEST(LedDriver, LedsOffAfterCreate) {
+  uint16_t virtual_leds = 0xffff;
+  LedDriver_Create(&virtual_leds);
+  TEST_ASSERT_EQUAL_HEX16(0, virtual_leds);
+}
+
 // A single LED can be turned on
 // A single LED can be turned off
 // Multiple LEDs can be turned on/off
